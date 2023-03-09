8d8186223a6ace09ed6b626a17543608deca3f87
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2023-03-09 21:33:24 +0100

    check for input method

--- core/src/com/nucleargame/classes/entity/Player.java
+++ core/src/com/nucleargame/classes/entity/Player.java
@@ -1,6 +1,7 @@
 package com.nucleargame.classes.entity;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 
 public class Player extends Entity{
@@ -11,5 +12,20 @@ public class Player extends Entity{
         rect.height=300;
         rect.x=100;
         rect.y=100;
+        movingSpeed=400;
+    }
+    public void checkForInput(){
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT) && !Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            rect.x -= movingSpeed * Gdx.graphics.getDeltaTime();
+        }
+        else if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)  && !Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            rect.x += movingSpeed * Gdx.graphics.getDeltaTime();
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.UP) && !Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            rect.y += movingSpeed * Gdx.graphics.getDeltaTime();
+        }
+        else if(Gdx.input.isKeyPressed(Input.Keys.DOWN)  && !Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            rect.y -= movingSpeed * Gdx.graphics.getDeltaTime();
+        }
     }
 }
