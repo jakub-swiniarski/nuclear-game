f06c1b2df8a7e1f25669596f29602f8acec4964e
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2023-03-09 21:57:40 +0100

    font, fps counter

--- core/src/com/nucleargame/NuclearGame.java
+++ core/src/com/nucleargame/NuclearGame.java
@@ -1,19 +1,45 @@
 package com.nucleargame;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.nucleargame.classes.entity.Player;
+import com.nucleargame.classes.floor.Grass;
 
 public class NuclearGame extends ApplicationAdapter {
 	SpriteBatch batch;
+	FreeTypeFontGenerator generator;
+	FreeTypeFontGenerator.FreeTypeFontParameter parameter;
+	BitmapFont font24;
 	Player player;
+	Grass grass;
+	OrthographicCamera worldCam;
+	World world;
+	Stage stage;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		worldCam = new OrthographicCamera(1600,900);
+		worldCam.position.set(0,0, 0);
 		player=new Player();
+		grass=new Grass();
+
+		stage = new Stage();
+		world = new World(new Vector2(0,0),false);
+
+		//font
+		generator = new FreeTypeFontGenerator(Gdx.files.internal("font.otf"));
+		parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+		parameter.size = 24;
+		font24 = generator.generateFont(parameter);
 	}
 
 	@Override
@@ -23,9 +49,18 @@ public class NuclearGame extends ApplicationAdapter {
 		player.cam.position.set(player.rect.x+player.rect.width/2,player.rect.y+player.rect.height/2,0);
 		player.cam.update();
 
+		//affected by light
 		batch.begin();
+		batch.draw(grass.img, grass.rect.x, grass.rect.y);
 		batch.draw(player.img, player.rect.x, player.rect.y);
 		batch.end();
+
+		//not affected by light, hud
+		batch.setProjectionMatrix(stage.getBatch().getProjectionMatrix());
+		batch.begin();
+		font24.draw(batch, "FPS: "+Gdx.graphics.getFramesPerSecond(), 5, 20);
+		batch.end();
+
 		player.checkForInput();
 	}
 	
@@ -33,5 +68,10 @@ public class NuclearGame extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		player.img.dispose();
+		grass.img.dispose();
+		generator.dispose();
+		font24.dispose();
+		world.dispose();
+		stage.dispose();
 	}
 }
