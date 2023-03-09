ed4ae32bfb7934c0373c484a2b4a9483c3b6f8e1
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2023-03-09 21:47:16 +0100

    camera

--- core/src/com/nucleargame/classes/floor/Grass.java
+++ core/src/com/nucleargame/classes/floor/Grass.java
@@ -5,6 +5,6 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class Grass extends Floor{
     public Grass(){
-        //img = new Texture(Gdx.files.internal("grass.png"));
+        img = new Texture(Gdx.files.internal("grass.png"));
     }
 }
