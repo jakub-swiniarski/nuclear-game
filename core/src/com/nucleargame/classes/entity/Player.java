5c6f54405df6c1a599b424d6f532acbbb0b13d89
PanPole <77209709+PanPolePL@users.noreply.github.com>
2023-03-09 21:47:16 +0100

    camera

--- core/src/com/nucleargame/classes/entity/Player.java
+++ core/src/com/nucleargame/classes/entity/Player.java
@@ -2,17 +2,20 @@ package com.nucleargame.classes.entity;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 
 public class Player extends Entity{
+    public OrthographicCamera cam;
     public Player(){
         hp=100;
         img = new Texture(Gdx.files.internal("player.png"));
         rect.width=180;
         rect.height=300;
-        rect.x=100;
-        rect.y=100;
+        rect.x=0;
+        rect.y=0;
         movingSpeed=400;
+        cam = new OrthographicCamera(1600,900);
     }
     public void checkForInput(){
         if(Gdx.input.isKeyPressed(Input.Keys.LEFT) && !Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
