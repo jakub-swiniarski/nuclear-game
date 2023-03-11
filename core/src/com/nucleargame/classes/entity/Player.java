61c20fc6c2aa01e781644918e40fe084da1e10b3
PanPole <77209709+PanPolePL@users.noreply.github.com>
2023-03-11 20:36:17 +0100

    tuptacz

--- core/src/com/nucleargame/classes/entity/Player.java
+++ core/src/com/nucleargame/classes/entity/Player.java
@@ -9,7 +9,13 @@ public class Player extends Entity{
     public OrthographicCamera cam;
     public Player(){
         hp=100;
-        img = new Texture(Gdx.files.internal("player/playerD.png"));
+        img = new Texture(Gdx.files.internal("player/walkingD/0.png"));
+        for(int i=0; i<3; i++){
+            walkingR[i]=new Texture(Gdx.files.internal("player/walkingR/"+i+".png"));
+            walkingL[i]=new Texture(Gdx.files.internal("player/walkingL/"+i+".png"));
+            walkingU[i]=new Texture(Gdx.files.internal("player/walkingU/"+i+".png"));
+            walkingD[i]=new Texture(Gdx.files.internal("player/walkingD/"+i+".png"));
+        }
         rect.width=180;
         rect.height=300;
         rect.x=0;
@@ -20,15 +26,27 @@ public class Player extends Entity{
     public void checkForInput(){
         if(Gdx.input.isKeyPressed(Input.Keys.LEFT) && !Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             rect.x -= movingSpeed * Gdx.graphics.getDeltaTime();
+            frameCount++;
+            if(frameCount>=3) frameCount=0;
+            img=walkingL[frameCount];
         }
         else if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)  && !Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             rect.x += movingSpeed * Gdx.graphics.getDeltaTime();
+            frameCount++;
+            if(frameCount>=3) frameCount=0;
+            img=walkingR[frameCount];
         }
         if(Gdx.input.isKeyPressed(Input.Keys.UP) && !Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
             rect.y += movingSpeed * Gdx.graphics.getDeltaTime();
+            frameCount++;
+            if(frameCount>=3) frameCount=0;
+            img=walkingU[frameCount];
         }
         else if(Gdx.input.isKeyPressed(Input.Keys.DOWN)  && !Gdx.input.isKeyPressed(Input.Keys.UP)) {
             rect.y -= movingSpeed * Gdx.graphics.getDeltaTime();
+            frameCount++;
+            if(frameCount>=3) frameCount=0;
+            img=walkingD[frameCount];
         }
     }
 }
