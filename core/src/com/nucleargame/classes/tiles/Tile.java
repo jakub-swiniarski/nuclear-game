cb8c45d15800aa035fa25b478e70b798f78fd15e
PanPole <77209709+PanPolePL@users.noreply.github.com>
2023-03-12 17:41:54 +0100

    added id attribute to tile class

--- core/src/com/nucleargame/classes/tiles/Tile.java
+++ core/src/com/nucleargame/classes/tiles/Tile.java
@@ -4,11 +4,13 @@ import com.badlogic.gdx.graphics.Texture;
 
 import java.awt.*;
 
-public class Floor {
+public class Tile {
     public Rectangle rect;
     public Texture img;
+    public int id;
 
-    public Floor(){
+    public Tile(){
+        id=0;
         rect=new Rectangle();
         rect.width=160;
         rect.height=160;
