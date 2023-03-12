4a40433d556db1fe9b47c207254b8e6a6e3f4ee0
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2023-03-12 17:41:54 +0100

    added id attribute to tile class

--- core/src/com/nucleargame/classes/other/WorldGenPanPole.java
+++ core/src/com/nucleargame/classes/other/WorldGenPanPole.java
@@ -9,7 +9,7 @@ public class WorldGenPanPole {
     }
     public void generate(){
         world=new int[width][height];
-        //2d array of block ids
+        //2d array of tile ids
         //the actual tiles will be generated in the main game class using the previously generated array
         //trees and ores will be generated in the main game class
         //each grass tile will have a chance to generate a tree (stone tiles will generate ores)
--- core/src/com/nucleargame/classes/tiles/Grass.java
+++ core/src/com/nucleargame/classes/tiles/Grass.java
@@ -3,8 +3,9 @@ package com.nucleargame.classes.tiles;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 
-public class Grass extends Floor{
+public class Grass extends Tile {
     public Grass(){
+        id=1;
         img = new Texture(Gdx.files.internal("tiles/grass.png"));
         rect.x=0;
         rect.y=0;
