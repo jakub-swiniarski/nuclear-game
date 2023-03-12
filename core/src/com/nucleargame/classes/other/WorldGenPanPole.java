1269eb220812e07ac8960874bb74575ff1748c10
PanPole <77209709+PanPolePL@users.noreply.github.com>
2023-03-12 11:04:55 +0100

    panpole worldgen

---
+++ core/src/com/nucleargame/classes/other/WorldGenPanPole.java
@@ -0,0 +1,12 @@
+package com.nucleargame.classes.other;
+
+public class WorldGenPanPole {
+    public int width,height;
+
+    public void generate(){
+        //2d array of block ids
+        //the actual tiles will be generated in the main game class using the previously generated array
+        //trees and ores will be generated in the main game class
+        //each grass tile will have a chance to generate a tree (stone tiles will generate ores)
+    }
+}
