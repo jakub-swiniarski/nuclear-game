3008a59d0f4511f38fe5e6d097aa8566f3955818
PanPole <77209709+PanPolePL@users.noreply.github.com>
2023-03-13 17:15:44 +0100

    Update Tree.java

--- core/src/com/nucleargame/classes/Content/Tree.java
+++ core/src/com/nucleargame/classes/Content/Tree.java
@@ -6,7 +6,8 @@ import com.badlogic.gdx.graphics.Texture;
 public class Tree extends Content {
     public Tree(){
         id=1;
-        img=new Texture(Gdx.files.internal("content/tree.png"));
+        img=new Texture(Gdx.files.internal("content/tree.png")); //trzeba bedzie rozdzielic sprite na dwie czesci zeby gracz
+        //byl pod gorna czescia ale nad dolna
         rect.width=360;
         rect.height=480;
     }
