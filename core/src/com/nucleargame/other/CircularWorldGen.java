cfcfd67b9736d55c7e685c3c695b6f777448b77e
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2023-03-27 22:33:09 +0200

    2nd worldgen attempt

---
+++ core/src/com/nucleargame/other/CircularWorldGen.java
@@ -0,0 +1,15 @@
+package com.nucleargame.other;
+
+public class CircularWorldGen {
+    public int radius;
+    public int[][] tile;
+
+    public CircularWorldGen(){
+        radius=50;
+    }
+    public void generate(){
+        for(int i=0; i<radius/2; i++){
+            
+        }
+    }
+}
